add1ac8a01cb81a7445623ff497dc710b05b5f75
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-07-21 14:32:19 +0200

    Add Playwright configuration and update test utilities
    - Introduced Playwright configuration file for end-to-end testing.
    - Added Playwright test dependencies in package.json.
    - Created utility functions for screenshot matching and map loading.
    - Implemented MapCanvas and SelectionUi classes for interaction with the map.
    - Added screenshot generation test case.
    - Updated .gitignore to exclude local snapshots.
    - Removed unused screenshot file.

---
+++ src/test/end-to-end/test/screenshot.spec.ts
@@ -0,0 +1,76 @@
+///
+/// Copyright (C) 2023 con terra GmbH (info@conterra.de)
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///         http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
+import { test } from '@playwright/test';
+
+import { expectToMatchScreenshot } from './common/testUtils';
+import { MapCanvas } from "./components/map-canvas";
+
+// test used to generate a screenshot for the bundle documentation
+test('Create Screenshot for GitHub Page', async ({ page }) => {
+    await page.goto('http://localhost:9090/');
+
+    const canvas = new MapCanvas(page);
+    await canvas.loaded();
+
+    await page.getByRole('button', { name: 'New analysis' }).click();
+    await page.locator('canvas').click({
+        position: {
+            x: 871,
+            y: 336
+        }
+    });
+    await page.locator('canvas').click({
+        position: {
+            x: 531,
+            y: 499
+        }
+    });
+    await page.locator('canvas').click({
+        position: {
+            x: 992,
+            y: 380
+        }
+    });
+    await page.locator('canvas').click({
+        position: {
+            x: 923,
+            y: 509
+        }
+    });
+    await page.locator('canvas').click({
+        position: {
+            x: 798,
+            y: 536
+        }
+    });
+    await page.locator('canvas').dblclick({
+        position: {
+            x: 855,
+            y: 189
+        }
+    });
+    await page.locator('canvas').dblclick({
+        position: {
+            x: 855,
+            y: 189
+        }
+    });
+
+    await expectToMatchScreenshot(page, "screenshot.png", {
+        timeout: 10000
+    });
+});
